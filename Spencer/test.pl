8a3fdb3f3cc3bfb8084e645a8227af1b69bc5da7
James Spencer <james.eat.world@gmail.com>
2012-05-09 01:28:46 +0100

    cleaned up some code commenting added aswell, more still needed on this front. I have implemented a dirty hack to stop recusion overflows... this is far from ideal but it prevents system crashes.

--- Spencer/test.pl
+++ Spencer/test.pl
@@ -1,2 +1,10 @@
 testAddition(X, Y, Z) :-
 Z is X+Y.
+
+
+
+countDown(Remaining):-
+nl, write(' Remaing : ' - Remaining),nl,ttyflush,
+Remaining > 0,
+SubRemaing is Remaining-1, 
+countDown(SubRemaing).
